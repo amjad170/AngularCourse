4c0e838d8ab95330792e09d0cabaf87dc3331905
Amjad <amjadhossain17416762@gmail.com>
2022-10-29 01:02:53 +0600

    add new component

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  courses=[
+    {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'},
+  {id:101, name:'JavaScript for beginer',author:'Jnon Hekela',durarion:48,type: 'Free',price:0.00,ratig:4.5,image:'assets/courses/course-image-1.jpeg',
+  description:'loremlskajoirueqorfksjfowiuerioqurilkjsdoa;uewioruoiqwurohwkj'}
+  ];
+
+}
